6a0d084381060670057dedb92d605f07753ec226
Miguel Cobian <miguelcobian99@gmail.com>
2024-07-22 10:15:22 -0600

    Chore. Rename key length

--- src/utils/SecretKey.ts
+++ src/utils/SecretKey.ts
@@ -1,7 +1,7 @@
 export default class SecretKey {
     static generateKey() {
         const crypto = require('crypto');
-        const randomBytes = crypto.randomBytes(32);
-        return crypto.createHash('sha3-256').update(randomBytes).digest('hex');
+        const KEY_LENGTH_IN_BYTES = crypto.randomBytes(32);
+        return crypto.createHash('sha3-256').update(KEY_LENGTH_IN_BYTES).digest('hex');
     }
 }
